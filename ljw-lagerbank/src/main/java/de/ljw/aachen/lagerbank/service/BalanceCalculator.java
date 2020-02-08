90333b9ee20f6c91760a8ee896ef75db8b15858f
Joshua Jungen <joshua.jungen@outlook.de>
2020-02-08 23:17:23 +0100

    simplify balance calculator

--- ljw-lagerbank/src/main/java/de/ljw/aachen/lagerbank/service/BalanceCalculator.java
+++ ljw-lagerbank/src/main/java/de/ljw/aachen/lagerbank/service/BalanceCalculator.java
@@ -5,26 +5,27 @@ import de.ljw.aachen.lagerbank.domain.Money;
 import de.ljw.aachen.lagerbank.domain.Transaction;
 
 import java.util.Collection;
+import java.util.function.Predicate;
 import java.util.stream.Stream;
 
 class BalanceCalculator {
 
+
     static Money calculateBalance(AccountId accountId, Collection<Transaction> transactions) {
-        Money earnings = getEarnings(accountId, transactions).reduce(Money.of(0.0), Money::plus);
-        Money expenses = getExpenses(accountId, transactions).reduce(Money.of(0.0), Money::plus);
+        Predicate<Transaction> earned = transaction -> accountId.equals(transaction.getTarget());
+        Predicate<Transaction> spent = transaction -> accountId.equals(transaction.getSource());
+
+        Money earnings = sum(transactions, earned);
+        Money expenses = sum(transactions, spent);
+
         return earnings.minus(expenses);
     }
 
-    private static Stream<Money> getEarnings(AccountId accountId, Collection<Transaction> transactions) {
+    private static Money sum(Collection<Transaction> transactions, Predicate<Transaction> predicate) {
         return transactions.stream()
-                .filter(transaction -> accountId.equals(transaction.getTarget()))
-                .map(Transaction::getAmount);
-
+                .filter(predicate)
+                .map(Transaction::getAmount)
+                .reduce(Money.of(0.0), Money::plus);
     }
 
-    private static Stream<Money> getExpenses(AccountId accountId, Collection<Transaction> transactions) {
-        return transactions.stream()
-                .filter(transaction -> accountId.equals(transaction.getSource()))
-                .map(Transaction::getAmount);
-    }
 }
