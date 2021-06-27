b660448dcfedf693a03fd91b9e1b80c3f4040acb
Joshua Jungen <joshua.jungen@outlook.de>
2021-06-27 20:41:13 +0200

    add list of all balances to menu

--- client/src/main/java/de/ljw/aachen/client/controller/MenuController.java
+++ client/src/main/java/de/ljw/aachen/client/controller/MenuController.java
@@ -14,7 +14,10 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
@@ -25,16 +28,19 @@ import org.springframework.stereotype.Component;
 
 import java.net.URL;
 import java.text.MessageFormat;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.ResourceBundle;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
-public class MenuController {
+public class MenuController
+{
 
 
     private final AccountStore accountStore;
@@ -45,7 +51,8 @@ public class MenuController {
     private ResourceBundle resources;
 
     @FXML
-    void onCashUp(ActionEvent event) {
+    void onCashUp(ActionEvent event)
+    {
         var allTransactions = transactionStore.getTransactions();
         var availableCash = CashUp.cashUp(allTransactions);
 
@@ -58,15 +65,35 @@ public class MenuController {
     }
 
     @FXML
-    void onCheckBalance(ActionEvent event) {
+    void onCheckBalance(ActionEvent event)
+    {
         var accounts = accountStore.getAccounts();
         var transactions = transactionStore.getTransactions();
 
         Map<Account, Money> accountBalanceMap = getAccountMoneyMap(accounts, transactions);
+        var zeroBalance = new Money(0.00);
+        accountBalanceMap.entrySet().removeIf(entry -> entry.getValue().isGreaterThanOrEqual(zeroBalance));
+
+        displayAlert(resources.getString("balance.check.title"), accountBalanceMap);
+    }
+
+    @FXML
+    void onListBalance(ActionEvent event)
+    {
+        var accounts = accountStore.getAccounts();
+        var transactions = transactionStore.getTransactions();
+
+        Map<Account, Money> accountBalanceMap = getAccountMoneyMap(accounts, transactions);
+
+        displayAlert(resources.getString("balance.list.title"), accountBalanceMap);
+    }
+
+    private void displayAlert(String title, Map<Account, Money> accountBalanceMap)
+    {
         Node content = getBalanceCheckContentNode(accountBalanceMap);
 
         var info = new Alert(Alert.AlertType.INFORMATION, null, ButtonType.OK);
-        info.setTitle(resources.getString("balance.check.title"));
+        info.setTitle(title);
         info.setHeaderText(null);
         info.getDialogPane().setContent(content);
         info.setResizable(true);
@@ -74,36 +101,38 @@ public class MenuController {
         info.show();
     }
 
-    private Node getBalanceCheckContentNode(Map<Account, Money> accountBalanceMap) {
-        if (accountBalanceMap.isEmpty()) return new Label(resources.getString("balance.check.no.accounts.with.negative.balance"));
+    private Node getBalanceCheckContentNode(Map<Account, Money> accountBalanceMap)
+    {
+        if (accountBalanceMap.isEmpty())
+            return new Label(resources.getString("balance.check.no.accounts.with.negative.balance"));
 
         GridPane nameBalanceGrid = new GridPane();
         nameBalanceGrid.setHgap(15);
         nameBalanceGrid.setVgap(5);
 
-        var rowIndex = 0;
-        for (var entry : accountBalanceMap.entrySet()) {
-            nameBalanceGrid.addRow(rowIndex++,
-                    new Label(ComposeFullName.process(entry.getKey())),
-                    new Label(entry.getValue().formatWithCurrency()));
-        }
-
+        var rowIndex = new AtomicInteger();
+        accountBalanceMap.entrySet().stream()
+                         .sorted(Comparator.comparing(entry -> ComposeFullName.process(entry.getKey())))
+                         .forEach(entry -> {
+                             nameBalanceGrid.addRow(rowIndex.getAndIncrement(),
+                                     new Label(ComposeFullName.process(entry.getKey())),
+                                     new Label(entry.getValue().formatWithCurrency()));
+                         });
         return nameBalanceGrid;
     }
 
-    private Map<Account, Money> getAccountMoneyMap(List<Account> accounts, List<Transaction> transactions) {
-        var zeroBalance = new Money(0.00);
-        var accountBalanceMap = accounts.stream()
-                .collect(Collectors.toMap(Function.identity(),
-                        account -> CalculateBalance.process(account.getId(), transactions)
-                ));
-        accountBalanceMap.entrySet().removeIf(entry -> entry.getValue().isGreaterThanOrEqual(zeroBalance));
-        return accountBalanceMap;
+    private Map<Account, Money> getAccountMoneyMap(List<Account> accounts, List<Transaction> transactions)
+    {
+        return accounts.stream()
+                       .collect(Collectors.toMap(Function.identity(),
+                               account -> CalculateBalance.process(account.getId(), transactions)
+                       ));
     }
 
     @FXML
     @SneakyThrows
-    void onImport(ActionEvent event) {
+    void onImport(ActionEvent event)
+    {
         URL resource = AccountSelectionController.class.getClassLoader().getResource("fxml/import_accounts.fxml");
         FXMLLoader loader = new FXMLLoader(resource);
         loader.setController(importAccountsController);
